4f092d1f4965eb7aa156ae01a5f335543052683b
Mikolaj Konieczny <mk.nex23@gmail.com>
2026-03-09 20:12:01 +0100

    com1

---
+++ Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lab = new System.Windows.Forms.Label();
+            this.delete = new System.Windows.Forms.Button();
+            this.wykres = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.export = new System.Windows.Forms.Button();
+            this.import = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(232, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(423, 548);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(663, 2);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(400, 548);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Liczba kroków:";
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(3, 240);
+            this.lab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(38, 16);
+            this.lab.TabIndex = 4;
+            this.lab.Text = "Usuń";
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(7, 314);
+            this.delete.Margin = new System.Windows.Forms.Padding(4);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(217, 28);
+            this.delete.TabIndex = 6;
+            this.delete.Text = "usuń";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // wykres
+            // 
+            this.wykres.Location = new System.Drawing.Point(7, 476);
+            this.wykres.Margin = new System.Windows.Forms.Padding(4);
+            this.wykres.Name = "wykres";
+            this.wykres.Size = new System.Drawing.Size(217, 28);
+            this.wykres.TabIndex = 11;
+            this.wykres.Text = "Narysuj Wykres";
+            this.wykres.UseVisualStyleBackColor = true;
+            this.wykres.Click += new System.EventHandler(this.showChartButton_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Red;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.exit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.exit.Location = new System.Drawing.Point(7, 513);
+            this.exit.Margin = new System.Windows.Forms.Padding(4);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(217, 28);
+            this.exit.TabIndex = 12;
+            this.exit.Text = "Zamknij";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 282);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 22);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 262);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Numer Rekordu:";
+            // 
+            // export
+            // 
+            this.export.Location = new System.Drawing.Point(7, 389);
+            this.export.Margin = new System.Windows.Forms.Padding(4);
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(217, 28);
+            this.export.TabIndex = 19;
+            this.export.Text = "Zapisz dane do pliku";
+            this.export.UseVisualStyleBackColor = true;
+            this.export.Click += new System.EventHandler(this.export_Click);
+            // 
+            // import
+            // 
+            this.import.Location = new System.Drawing.Point(7, 354);
+            this.import.Margin = new System.Windows.Forms.Padding(4);
+            this.import.Name = "import";
+            this.import.Size = new System.Drawing.Size(217, 28);
+            this.import.TabIndex = 20;
+            this.import.Text = "Pobierz dane z pliku";
+            this.import.UseVisualStyleBackColor = true;
+            this.import.Click += new System.EventHandler(this.import_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Data:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 176);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 22);
+            this.textBox2.TabIndex = 23;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(7, 208);
+            this.add.Margin = new System.Windows.Forms.Padding(4);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(217, 28);
+            this.add.TabIndex = 22;
+            this.add.Text = "Dodaj";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.addRowButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Dodaj";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 156);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Dystans";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(7, 129);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(216, 22);
+            this.dateTimePicker1.TabIndex = 26;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 37);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 16);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Liczba kilometrów:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 425);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.import);
+            this.Controls.Add(this.export);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.wykres);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button wykres;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button export;
+        private System.Windows.Forms.Button import;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace projekt
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dt;
+        private DataCalculator dataCalculator;
+
+        public class DataCalculator
+        {
+            private DataTable dataTable;
+
+            public DataCalculator(DataTable dataTable)
+            {
+                this.dataTable = dataTable;
+            }
+            public int GetTotalDistance()
+            {
+                return (int)dataTable.AsEnumerable()
+                    .Where(row => row.Field<object>("Dystans(km)") != DBNull.Value)
+                    .Sum(row => row.Field<double>("Dystans(km)"));
+            }
+            public int GetTotalSteps()
+            {
+                int totalDistance = GetTotalDistance();
+                return totalDistance * 1430;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            DataColumn idColumn = new DataColumn("ID", typeof(int));
+            idColumn.AutoIncrement = true;
+            idColumn.AutoIncrementSeed = 1;
+            idColumn.AutoIncrementStep = 1;
+            dt.Columns.Add(idColumn);
+            dt.Columns.Add(new DataColumn("Data", typeof(DateTime)));
+            dt.Columns.Add(new DataColumn("Dystans(km)", typeof(double)));
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                dt.Rows.Add(null, DateTime.Today.AddDays(+i), rnd.Next(1, 100));
+            }
+
+            dataCalculator = new DataCalculator(dt);
+
+            chart1.Series[0].IsVisibleInLegend = false;
+            dataGridView1.DataSource = dt;
+
+            int distance = dataCalculator.GetTotalDistance();
+            int steps = dataCalculator.GetTotalSteps();
+
+            label1.Text ="Liczba kroków: "+ steps.ToString();
+            label6.Text ="Liczba kilometrów: "+ distance.ToString();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Zamykanie aplikacji.");
+            this.Close();
+        }
+
+        private void addRowButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Pobranie danych z pól tekstowych
+                if (DateTime.TryParse(dateTimePicker1.Text, out DateTime parsedDate) && double.TryParse(textBox2.Text, out double parsedDystans))
+                {
+                    dt.Rows.Add(null, parsedDate, parsedDystans);
+                }
+                else
+                {
+                    MessageBox.Show("Nieprawidłowe dane. Upewnij się, że wprowadzono poprawną datę i dystans.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wystąpił błąd podczas dodawania nowego wiersza: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            dataCalculator = new DataCalculator(dt);
+
+            int distance = dataCalculator.GetTotalDistance();
+            int steps = dataCalculator.GetTotalSteps();
+
+            label1.Text = "Liczba kroków: " + steps.ToString();
+            label6.Text = "Liczba kilometrów: " + distance.ToString();
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            // Usuwanie rekordu o podanym numerze ID
+            string input = textBox1.Text;
+
+            if (int.TryParse(input, out int idToDelete))
+            {
+                DataRow rowToDelete = dt.AsEnumerable().FirstOrDefault(row => row.Field<int>("ID") == idToDelete);
+
+                if (rowToDelete != null)
+                {
+                    dt.Rows.Remove(rowToDelete);
+                }
+                else
+                {
+                    MessageBox.Show($"Rekord o ID {idToDelete} nie istnieje.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Podano nieprawidłowy numer ID.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            dataCalculator = new DataCalculator(dt);
+
+            int distance = dataCalculator.GetTotalDistance();
+            int steps = dataCalculator.GetTotalSteps();
+
+            label1.Text = "Liczba kroków: " + steps.ToString();
+            label6.Text = "Liczba kilometrów: " + distance.ToString();
+        }
+
+        private void export_Click(object sender, EventArgs e)
+        {
+            // Eksport danych do pliku CSV
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv",
+                Title = "Zapisz jako"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    // Nagłówki kolumn
+                    var columnNames = dt.Columns.Cast<DataColumn>().Select(column => column.ColumnName);
+                    writer.WriteLine(string.Join(",", columnNames));
+
+                    // Dane w wierszach
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var fields = row.ItemArray.Select(field => field.ToString());
+                        writer.WriteLine(string.Join(",", fields));
+                    }
+                }
+
+                MessageBox.Show("Dane zostały zapisane do pliku CSV.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            dataCalculator = new DataCalculator(dt);
+
+            int distance = dataCalculator.GetTotalDistance();
+            int steps = dataCalculator.GetTotalSteps();
+
+            label1.Text = "Liczba kroków: " + steps.ToString();
+            label6.Text = "Liczba kilometrów: " + distance.ToString();
+        }
+
+        private void import_Click(object sender, EventArgs e)
+        {
+            // Import danych z pliku CSV
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv",
+                Title = "Otwórz plik CSV"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                    {
+                        string headerLine = reader.ReadLine();
+                        string[] headers = headerLine.Split(',');
+
+                        DataTable importTable = new DataTable();
+                        foreach (string header in headers)
+                        {
+                            importTable.Columns.Add(new DataColumn(header));
+                        }
+
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            string[] values = line.Split(',');
+                            importTable.Rows.Add(values);
+                        }
+
+                        dt.Clear(); // Clear the existing data
+                        foreach (DataColumn column in importTable.Columns)
+                        {
+                            if (!dt.Columns.Contains(column.ColumnName))
+                            {
+                                dt.Columns.Add(new DataColumn(column.ColumnName));
+                            }
+                        }
+
+                        foreach (DataRow row in importTable.Rows)
+                        {
+                            dt.Rows.Add(row.ItemArray);
+                        }
+
+                        MessageBox.Show("Dane zostały zaimportowane i zastąpiły istniejące dane.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Wystąpił błąd podczas importu danych: {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                dataCalculator = new DataCalculator(dt);
+
+                int distance = dataCalculator.GetTotalDistance();
+                int steps = dataCalculator.GetTotalSteps();
+
+                label1.Text = "Liczba kroków: " + steps.ToString();
+                label6.Text = "Liczba kilometrów: " + distance.ToString();
+            }
+        }
+
+        private void showChartButton_Click(object sender, EventArgs e)
+        {
+            // Wyświetlenie danych w postaci wykresu słupkowego
+            chart1.Series.Clear();
+            Series series = new Series("Dystans(km)")
+            {
+                ChartType = SeriesChartType.Column,
+                XValueType = ChartValueType.DateTime
+            };
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if (row["Data"] != DBNull.Value && row["Dystans(km)"] != DBNull.Value)
+                {
+                    series.Points.AddXY(Convert.ToDateTime(row["Data"]), Convert.ToDouble(row["Dystans(km)"]));
+                }
+            }
+
+            chart1.Series.Add(series);
+            chart1.Series[0].IsVisibleInLegend = false;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            dataCalculator = new DataCalculator(dt);
+
+            int distance = dataCalculator.GetTotalDistance();
+            int steps = dataCalculator.GetTotalSteps();
+
+            label1.Text = "Liczba kroków: " + steps.ToString();
+            label6.Text = "Liczba kilometrów: " + distance.ToString();
+        }
+    }
+}
